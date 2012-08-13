625787ba575830a1ce48b37e2759eaff184040f9
Ward Muylaert <ward.muylaert@gmail.com>
2012-08-13 11:12:01 -0400

    makeissue based to create /etc/issue
    Based on https://bbs.archlinux.org/viewtopic.php?id=50845&p=1
    Minor adjustments to one of the examples in that thread.

---
+++ makeissue.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo -e '\e[H\e[2J'
+echo
+echo
+echo -e '                           \e[1;36m`'
+echo -e '                          \e[1;36m-:'
+echo -e '                         \e[1;36m.//:'
+echo -e '                        \e[1;36m`////-'
+echo -e '                       \e[1;36m`://///.'
+echo -e '                       \e[1;36m:///////.'
+echo -e '                      \e[1;36m-/////////.'
+echo -e '                     \e[1;36m`://////////`'
+echo -e '                    \e[1;36m-:..://///////`'
+echo -e '                   \e[1;36m-////:::////////`'
+echo -e '                  \e[1;36m-/////////////////`'
+echo -e '                 \e[1;36m-//////////++++/////`'
+echo -e '                \e[1;36m-////\e[0;36m++++oooooooooo+++.'
+echo -e '               \e[0;36m-/+++oooooooooooooooooo+.     \e[1;37m//=========================---\e[0m'
+echo -e '              \e[0;36m:+oooooooo+-..-/+oooooooo+.     \e[1;37m\\\\ \e[1;34m\s(\m) \\r\e[0m'
+echo -e '            \e[0;36m`/ooooooooo:`     .+oooooooo+.     \e[1;37m\\\\\e[0m'
+echo -e '           \e[0;36m`/ooooooooo/        .ooooooooo+-     \e[1;37m\\\\ \e[1;32m\\t\e[0m'
+echo -e '          \e[0;36m`/oooooooooo`         /oooooo++++-     \e[1;37m\\\\ \e[1;32m\d\e[0m'
+echo -e '         \e[0;36m`+ooooooooooo`         :oooooo++/-:.     \e[1;37m\\\\\e[0m'
+echo -e '        \e[0;36m.+ooooooooooo+`         :+oooooooo+/-`     \e[1;37m\\\\ \e[1;31m\U\e[0m'
+echo -e '       \e[0;36m.+oooooo++/:-.`          `..-:/++ooooo+:     \e[1;37m\\\\\e[0m'
+echo -e '      \e[0;36m.+oo++/-.`                      `.-:++ooo:     \e[1;37m\\\\ \e[0;35m\l \e[0;37mon \e[0;33m\\n\e[0m'
+echo -e '     \e[0;36m-++/-.`     \e[01;37mA simple, lightweight     \e[0;36m`-:++/`    \e[1;37m\\\\=========================---\e[0m'
+echo -e '     \e[0;36m-++/-.`      \e[1;37mlinux distribution.      \e[0;36m`-:++/`     '
+echo -e '    \e[0;36m.:.`                                       .--      \e[0m'
+echo
+echo
