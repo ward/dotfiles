bd6e9db6d0cf7daadc0b97b7f4969de34572b024
Ward Muylaert <ward.muylaert@gmail.com>
2015-10-18 11:40:06 +0200

    fr10 backup to dropbox script

---
+++ bin/fr10backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Shows how full/empty this filesystem is getting
+# -h human readable
+df -h .
+
+# Backs up any new files to the Dropbox folder
+# -v, --verbose               increase verbosity
+# -u, --update                skip files that are newer on the receiver
+# -t, --times                 preserve modification times
+# -h, --human-readable        output numbers in a human-readable format
+#     --progress              show progress during transfer
+rsync -h -v -t --progress -u * ~/Dropbox/docs/run/forerunner10
