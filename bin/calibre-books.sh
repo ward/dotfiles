7cc99a1f4fbdc7599ac8e89d34782b0baa278684
Ward Muylaert <ward.muylaert@gmail.com>
2019-04-20 09:54:47 +0200

    Make calibre-books script less spammy

--- bin/calibre-books.sh
+++ bin/calibre-books.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-set -o xtrace
-
 HOST=pi
 REMOTESRC=/mnt/seagate-1tb/books/en
 LOCALSRC=/mnt/pi-books
 
 if [ "$1" == "mount" ]
 then
-  sshfs $HOST:$REMOTESRC $LOCALSRC
+  (set -x; sshfs $HOST:$REMOTESRC $LOCALSRC)
 fi
 
 if [ "$1" == "unmount" ]
 then
-  fusermount -u $LOCALSRC
+  (set -x; fusermount -u $LOCALSRC)
 fi
